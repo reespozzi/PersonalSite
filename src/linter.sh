f31b07170a58a65456710c2ce5f5928cd2b9272e
Rees Pozzi <rees.pozzi@kainos.com>
2022-04-04 11:22:09 +0100

    Push basic failing file check

--- src/linter.sh
+++ src/linter.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Hello World!"
+echo "Starting generalised file checks!"
+if [[ !(-f "404.html") || !(-f "index.html") || !(-d "resources") || !(-f "test") ]]; then
+    echo "One of the required files was missing, aborting deployment." >&2
+fi
