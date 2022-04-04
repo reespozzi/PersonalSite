a952ca3da2d43e017c2c7c48016ff1f820acff2d
Rees Pozzi <rees.pozzi@kainos.com>
2022-04-04 11:24:40 +0100

    Push failing file error

--- src/linter.sh
+++ src/linter.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 echo "Starting generalised file checks!"
+
+
 if [[ !(-f "404.html") || !(-f "index.html") || !(-d "resources") || !(-f "test") ]]; then
     echo "One of the required files was missing, aborting deployment." >&2
+    exit 1
 fi
