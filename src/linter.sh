2e8dad5aea6cc8e077a4e78fb1805e11305a9be6
Rees Pozzi <rees.pozzi@kainos.com>
2022-04-04 11:29:36 +0100

    Removed testing feature

--- src/linter.sh
+++ src/linter.sh
@@ -3,7 +3,7 @@
 echo "Starting generalised file checks!"
 
 
-if [[ !(-f "404.html") || !(-f "index.html") || !(-d "resources") || !(-f "test") ]]; then
+if [[ !(-f "404.html") || !(-f "index.html") || !(-d "resources") ]]; then
     echo "One of the required files was missing, aborting deployment." >&2
     exit 1
 fi
